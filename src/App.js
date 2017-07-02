f904becee8a3d567e0515dff8fb56f58a1f4de9c
nickmyers217 <nickbmyers217@gmail.com>
2017-07-01 20:28:39 -0500

    Feat: book status chaning now works

--- src/App.js
+++ src/App.js
@@ -19,9 +19,11 @@ const SearchPage = ({ onBackBtnClick }) => (
   </div>
 );
 
-const StatusSelect = ({ statuses }) => (
+const StatusSelect = ({ statuses, value, onStatusChange }) => (
   <div className="book-shelf-changer">
-    <select>
+    <select 
+      value={value}
+      onChange={e => onStatusChange(e.target.value)}>
       <option value="none" disabled>Move to...</option>
       {Object.keys(statuses).map(k =>
         <option key={k} value={statuses[k].value}>
@@ -32,19 +34,21 @@ const StatusSelect = ({ statuses }) => (
   </div>
 );
 
-const Book = ({ book, statuses }) => {
+const Book = ({ book, statuses, onStatusChange }) => {
   const { imageLinks, title = '', authors = [], width, height } = book;
   return (
     <div className="book">
       <div className="book-top">
-        <div className="book-cover"
-          style={{
+        <div className="book-cover" style={{
             width: width ? width : 128,
             height: height ? height : 193,
             backgroundImage: `url("${imageLinks.thumbnail}")`
           }}>
         </div>
-        <StatusSelect statuses={statuses} />
+        <StatusSelect
+          statuses={statuses}
+          value={book.status.value}
+          onStatusChange={onStatusChange(book.id)} />
       </div>
       <div className="book-title">{title}</div>
       <div className="book-authors">{authors.join(', ')}</div>
@@ -52,13 +56,15 @@ const Book = ({ book, statuses }) => {
   );
 };
 
-const BookShelf = ({ title, books, statuses }) => (
+const BookShelf = ({ title, books, statuses, onBookStatuschange }) => (
   <div className="bookshelf">
     <h2 className="bookshelf-title">{title}</h2>
     <div className="bookshelf-books">
       <ol className="books-grid">
-        {books.map((book, i) =>
-          <li key={i}><Book book={book} statuses={statuses} /></li>)}
+        {books.map(book =>
+          <li key={book.id}>
+            <Book book={book} statuses={statuses} onStatusChange={onBookStatuschange} />
+          </li>)}
       </ol>
     </div>
   </div>
@@ -67,9 +73,9 @@ const BookShelf = ({ title, books, statuses }) => (
 class BooksApp extends React.Component {
   constructor(props) {
     super(props);
-
+    this.changeBookStatus = this.changeBookStatus.bind(this);
     this.state = {
-      searchPhrase: 'Redux',
+      searchPhrase: 'Baseball',
       statuses: props.statuses,
       books: []
     };
@@ -91,6 +97,30 @@ class BooksApp extends React.Component {
 
   getBooksInStatus(status) {
     return this.state.books.filter(b => b.status === status);
+  }
+
+  changeBookStatus(bookId) {
+    return newStatusValue => {
+      let books = [];
+
+      if (newStatusValue === 'none') {
+        books = this.state.books.filter(b => b.id !== bookId);
+      } else {
+        const newStatus = Object.keys(this.state.statuses)
+          .map(k => this.state.statuses[k])
+          .filter(status => status.value === newStatusValue)
+          .shift();
+
+        books = this.state.books
+          .map(book => (
+            book.id === bookId
+              ? { ...book, status: newStatus }
+              : book
+          ));
+      }
+
+      this.setState(prevState => ({ ...prevState, books }));
+    };
   }
 
   componentDidMount() {
@@ -115,7 +145,8 @@ class BooksApp extends React.Component {
                       key={k}
                       title={statuses[k].display}
                       books={this.getBooksInStatus(statuses[k])}
-                      statuses={statuses} />)}
+                      statuses={statuses}
+                      onBookStatuschange={this.changeBookStatus} />)}
                 </div>
               </div>
               <div className="open-search">
